cf402fa1787c9f82efba0122d2ef741115798b0d
Jesse Chen <ruixiang.chen@bizops.com.cn>
2024-08-09 17:32:41 +0800

    proxy

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,40 +1,18 @@
 #include "./client.hpp"
 
-#include <iostream>
-#include <cstring>
-#include <sys/socket.h>
+#include "fcntl.h"
 #include <arpa/inet.h>
+#include <cstring>
+#include <iostream>
+#include <sys/socket.h>
+#include <thread>
 #include <unistd.h>
-void client::start(){
+#include <vector>
 
-     int sock = 0, valread;
-    struct sockaddr_in serv_addr;
-    const char* hello = "Hello from client";
-    char buffer[1024] = {0};
 
-    // 创建套接字
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        std::cerr << "Socket creation error" << std::endl;
-    }
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(8080);
 
-    // 将IP地址从字符串转换为网络地址
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
-        std::cerr << "Invalid address/ Address not supported" << std::endl;
-    }
+void client::start() {
+  
 
-    // 连接到服务器
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        std::cerr << "Connection Failed" << std::endl;
-    }
-
-    // 发送消息到服务器
-    send(sock, hello, strlen(hello), 0);
-    std::cout << "Hello message sent to server" << std::endl;
-
-    // 接收服务器响应
-    valread = read(sock, buffer, 1024);
-    std::cout << "Server response: " << buffer << std::endl;
 }
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -7,50 +7,121 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <thread>
+
+static int proxy_server_fd, public_server_fd, remote_client_socket, proxy_request_socket;
+
+
+
+void proxyHandler(int client_socket) {
+    char buffer[1024] = {0};
+    const char* response = "Message received by server\n";
+    // 接收客户端消息
+    read(client_socket, buffer, 1024);
+    std::cout << "Message from client: " << buffer << std::endl;
+    // 发送响应消息到客户端
+    send(remote_client_socket, buffer, 1024, 0);
+}
+
+
+void clientHandler(int client_socket) {
+    char buffer[1024] = {0};
+    const char* response = "Message received by server\n";
+    // 接收客户端消息
+    read(client_socket, buffer, 1024);
+    std::cout << "Message from client: " << buffer << std::endl;
+    // 发送响应消息到客户端
+    send(remote_client_socket, buffer, 1024, 0);
+}
+
 
 void server::start() {
-        int server_fd, new_socket;
+        
     struct sockaddr_in address;
+    struct sockaddr_in public_server_address;
     int addrlen = sizeof(address);
     int opt = 1;
     char buffer[1024] = {0};
     const char* hello = "Hello from server";
 
     // 创建套接字
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((proxy_server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((public_server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
     // 设置套接字选项
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    if (setsockopt(proxy_server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))
+    || setsockopt(public_server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(8080);
+    address.sin_port = htons(1997);
+
+    public_server_address.sin_family = AF_INET;
+    public_server_address.sin_addr.s_addr = INADDR_ANY;
+    public_server_address.sin_port = htons(1989);
 
     // 绑定套接字
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+    if (bind(proxy_server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (bind(public_server_fd, (struct sockaddr *)&public_server_address, sizeof(public_server_address))<0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
 
     // 监听
-    if (listen(server_fd, 3) < 0) {
+    if (listen(proxy_server_fd, 3) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
-    // 接受连接
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
-        perror("accept");
+    // 监听
+    if (listen(public_server_fd, 3) < 0) {
+        perror("listen");
         exit(EXIT_FAILURE);
     }
 
+    // // 接受连接
+    // if ((remote_client_socket = accept(proxy_server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+    //     perror("accept");
+    //     exit(EXIT_FAILURE);
+    // }
+
+     while (1) {
+        // 接受连接
+        if ((proxy_request_socket = accept(public_server_fd, (struct sockaddr *)&public_server_address, (socklen_t*)&addrlen))<0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        // 创建新线程处理客户端请求
+        std::thread clientThread(proxyHandler, proxy_request_socket);
+        clientThread.detach(); // 分离线程，使其在后台运行
+    }
+
+    while (1) {
+        // 接受连接
+        if ((remote_client_socket = accept(proxy_server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        // 创建新线程处理客户端请求
+        std::thread clientThread(clientHandler, proxy_request_socket);
+        clientThread.detach(); // 分离线程，使其在后台运行
+    }
+
     // 发送消息到客户端
-    send(new_socket, hello, strlen(hello), 0);
+    send(remote_client_socket, hello, strlen(hello), 0);
     std::cout << "Hello message sent\n";
 }
