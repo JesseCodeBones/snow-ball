71a9f845cf5635b0256146054af78e1d47a89f44
Jesse Chen <ruixiang.chen@bizops.com.cn>
2024-08-13 16:10:00 +0800

    add server proxy

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -1,7 +1,7 @@
 
 #include "./server.hpp"
-#include <chrono>
 #include <arpa/inet.h>
+#include <chrono>
 #include <cstdint>
 #include <iomanip>
 #include <iostream>
@@ -44,8 +44,8 @@ void server::start() {
   while ((client_fd = accept(listen_fd, (struct sockaddr *)&client_addr,
                              &client_addr_len)) > 0) {
     struct sockaddr_in client_addr;
-    uint32_t client_addr_len ;
-    getpeername(client_fd, (struct sockaddr*)&client_addr, &client_addr_len);
+    uint32_t client_addr_len;
+    getpeername(client_fd, (struct sockaddr *)&client_addr, &client_addr_len);
 
     char ip_str[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &client_addr.sin_addr, ip_str, sizeof(ip_str));
@@ -74,8 +74,12 @@ void server::start() {
       std::cout << "Peer port: " << port << std::endl;
       close(client_fd);
       continue;
+    } else {
+      remote_fd = client_fd;
+      remote_host = ip_str;
+      remote_port = port;
+      proxy();
     }
-
 
     while ((n = recv(client_fd, buffer, sizeof(buffer), 0)) > 0) {
       auto now = std::chrono::system_clock::now();
@@ -84,8 +88,100 @@ void server::start() {
       std::this_thread::sleep_for(std::chrono::seconds(1));
       send(client_fd, message.data(), message.size(), 0);
       std::string_view message(buffer, n);
-      std::cout << std::put_time(local_time, "%Y-%m-%d %H:%M:%S") << " - " << "heart beat from: " << ip_str << ":" << port << " - "
+      std::cout << std::put_time(local_time, "%Y-%m-%d %H:%M:%S") << " - "
+                << "heart beat from: " << ip_str << ":" << port << " - "
                 << message << std::endl;
     }
   }
+}
+
+void handleRequest(int from_fd, int to_fd) {
+  // 数据转发
+  char buffer[1024 * 50];
+  int n;
+  while (1) {
+    n = recv(from_fd, buffer, sizeof(buffer), 0);
+    std::cout << "handle data:" << n << std::endl;
+    if (n <= 0) {
+      return;
+    }
+    std::cout << "from:" << from_fd << "to:" << to_fd << std::endl;
+    send(to_fd, buffer, n, 0);
+  }
+}
+
+void server::async_handle(int client_fd, std::string_view remote_host,
+                          int remote_port) {
+  if (client_fd < 0) {
+    return;
+  }
+  int remote_fd;
+  struct sockaddr_in remote_addr;
+  // 连接远程服务器
+  remote_fd = socket(AF_INET, SOCK_STREAM, 0);
+  std::cout << "Connected to remote server = " << remote_fd << std::endl;
+  remote_addr.sin_family = AF_INET;
+  remote_addr.sin_addr.s_addr = inet_addr(remote_host.data());
+  remote_addr.sin_port = htons(remote_port);
+
+  if (connect(remote_fd, (struct sockaddr *)&remote_addr, sizeof(remote_addr)) <
+      0) {
+    std::cerr << "Failed to connect to remote server" << std::endl;
+    close(client_fd);
+    return;
+  }
+
+  std::thread t1(handleRequest, client_fd, remote_fd);
+  std::thread t2(handleRequest, remote_fd, client_fd);
+  t1.join();
+  t2.join();
+  std::cout << "disconnect = " << remote_fd << std::endl;
+  std::cout << "disconnect = " << client_fd << std::endl;
+  close(remote_fd);
+  close(client_fd);
+}
+
+void server::proxy() {
+
+  int listen_fd, client_fd, remote_fd;
+  struct sockaddr_in local_proxy_addr, remote_addr, client_addr;
+  socklen_t client_addr_len = sizeof(client_addr);
+  // 创建本地监听套接字
+  listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (listen_fd < 0) {
+    std::cerr << "Failed to create listen socket" << std::endl;
+    return;
+  }
+  // 绑定本地地址和端口
+  local_proxy_addr.sin_family = AF_INET;
+  local_proxy_addr.sin_addr.s_addr = INADDR_ANY;
+  local_proxy_addr.sin_port = htons(3366);
+
+  if (bind(listen_fd, (struct sockaddr *)&local_proxy_addr, sizeof(local_proxy_addr)) < 0) {
+    std::cerr << "Failed to bind" << std::endl;
+    close(listen_fd);
+    return;
+  }
+
+  // 开始监听
+  if (listen(listen_fd, 5) < 0) {
+    std::cerr << "Failed to listen" << std::endl;
+    close(listen_fd);
+    return;
+  }
+
+  std::cout << "Listening on port 8080..." << std::endl;
+
+  while (true) {
+    // 接受客户端连接
+    client_fd =
+        accept(listen_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+    std::cout << "Accepted client connection = " << client_fd << std::endl;
+    if (client_fd < 0) {
+      std::cerr << "Failed to accept client connection" << std::endl;
+      continue;
+    }
+    std::thread(async_handle, client_fd).detach();
+  }
+  close(listen_fd);
 }
--- src/server/server.hpp
+++ src/server/server.hpp
@@ -1,5 +1,6 @@
 #ifndef _snow_ball_server_
 #define _snow_ball_server_
+#include <string>
 
 class server {
 public:
@@ -7,5 +8,9 @@ public:
 
 private:
   int remote_fd = -1;
+  std::string remote_host;
+  int remote_port = -1;
+  void proxy();
+  static void async_handle(int client_fd, std::string_view remote_host, int remote_port);
 };
 #endif
